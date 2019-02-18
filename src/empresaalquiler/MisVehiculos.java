f91a989e8f36ad6c5863eec6dd9a9c7900a7645c
JCarlos F. Vico <35899915+jfervic933@users.noreply.github.com>
2019-02-18 12:43:56 +0100

    Update MisVehiculos.java
    Penalización por no tabular

--- src/empresaalquiler/MisVehiculos.java
+++ src/empresaalquiler/MisVehiculos.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*Penalización en la nota de dos puntos por no tabular el código*/
 package empresaalquiler;
 
 /**
